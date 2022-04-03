dfb8837e9e210ca63937cd926dea021ca29607b3
ganadosx <przemek4121@gmail.com>
2022-04-03 14:37:59 +0200

    Initial commit

---
+++ src/main/java/com/koapti/lazychef/LazychefApplication.java
@@ -0,0 +1,13 @@
+package com.koapti.lazychef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazychefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazychefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koapti/lazychef/LazychefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koapti.lazychef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazychefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
