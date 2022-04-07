8aa54db527d9df67afb62055e6370c7882553af1
ganadosx <przemek4121@gmail.com>
2022-04-07 23:28:07 +0200

    applicationProperties

--- src/main/java/com/koapti/lazychef/LazychefApplication.java
+++ src/main/java/com/koapti/lazychef/LazychefApplication.java
@@ -2,8 +2,12 @@ package com.koapti.lazychef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+// TYMCZASOWY EXCLUDE
+// do zastanowienia, czy uzywamy JPA czy JDBC, jesli JPA to bedzie trzeba naprawiac i usunac excluda
+// ale chyba bedziemy korzystac z hibernate, wiec moze byc problem
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 public class LazychefApplication {
 
 	public static void main(String[] args) {
