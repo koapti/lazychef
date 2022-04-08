e31b8bd3ec87681b126bcdff5379550c64ef4049
ganadosx <przemek4121@gmail.com>
2022-04-08 20:56:15 +0200

    DB settings, connected with swagger

--- src/main/java/com/koapti/lazychef/LazychefApplication.java
+++ src/main/java/com/koapti/lazychef/LazychefApplication.java
@@ -2,16 +2,14 @@ package com.koapti.lazychef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-// TYMCZASOWY EXCLUDE
-// do zastanowienia, czy uzywamy JPA czy JDBC, jesli JPA to bedzie trzeba naprawiac i usunac excluda
-// ale chyba bedziemy korzystac z hibernate, wiec moze byc problem
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@EnableSwagger2
+@SpringBootApplication()
 public class LazychefApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LazychefApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LazychefApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/koapti/lazychef/configuration/swagger/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.koapti.lazychef.configuration.swagger;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(createApiInfo())
+                .securitySchemes(Collections.singletonList(createSchema()))
+                .securityContexts(Collections.singletonList(createContext()));
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Lazychef",
+                "Projekt zespolowy",
+                "1.0.0",
+                "#",
+                new Contact("Lazychef", "#", "#"),
+                "License",
+                "#",
+                Collections.emptyList()
+        );
+    }
+
+    private SecurityContext createContext() {
+        return SecurityContext.builder()
+                .securityReferences(createRef())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityScheme createSchema() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private List<SecurityReference> createRef() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
+}
