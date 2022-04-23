4502b9159a646fb1dc04f8aeb7a59e91ae00fe1c
ganados <przemek4121@gmail.com>
2022-04-23 19:46:22 +0200

    Static code analysis

--- src/main/java/com/koapti/lazychef/model/entity/Order.java
+++ src/main/java/com/koapti/lazychef/model/entity/Order.java
@@ -12,7 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.koapti.lazychef.model.states.FoodState;
 import com.koapti.lazychef.model.states.OrderState;
 
 @Entity
